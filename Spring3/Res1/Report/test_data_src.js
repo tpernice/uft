36755d9eb929c4039b15b500b2e42c15834be472
Tommaso <tpernice@hotmail.com>
2017-10-05 13:23:54 +0000

    ok

---
+++ Spring3/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\tpernice\\Documents\\Unified Functional Testing\\Spring3\\Res1',namePairs:[]}]};
